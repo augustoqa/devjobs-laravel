a03ab472a29a1c58941c868596ebc1007a2c776a
Cesar Acual <augustoacual@gmail.com>
2021-02-22 11:56:39 -0600

    Ajustar editor en la vista create de vacantes

--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -129,12 +129,13 @@
 
         <div class="mb-5">
             <label
-                for="salario"
+                for="descripcion"
                 class="block text-gray-700 text-sm mb-2"
             >
                 Descripción del Puesto:
             </label>
-            <div class="editable"></div>
+            <div class="editable p-3 bg-gray-100 rounded form-input w-full text-gray-700"></div>
+            <input type="hidden" name="descripcion" id="descripcion">
         </div>
 
         <button
@@ -150,7 +151,21 @@
     <script src="//cdn.jsdelivr.net/npm/medium-editor@5.23.2/dist/js/medium-editor.min.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', () => {
-            const editor = new MediumEditor('.editable')
+            const editor = new MediumEditor('.editable', {
+                toolbar: {
+                    buttons: ['bold', 'italic', 'underline', 'quote', 'anchor', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'orderedList', 'unorderedList', 'h2', 'h3'],
+                    static: true,
+                    sticky: true
+                },
+                placeholder: {
+                    text: 'Información de la vacante'
+                }
+            })
+
+            editor.subscribe('editableInput', function (eventObj, editable) {
+                const contenido = editor.getContent()
+                document.querySelector('#descripcion').value = contenido
+            })
         })
     </script>
 @endsection
