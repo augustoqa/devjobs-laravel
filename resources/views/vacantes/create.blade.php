a06eefe6070977e33227ed3f5199ed8ba0133c5d
Cesar Acual <augustoacual@gmail.com>
2021-03-16 22:23:38 -0600

    Validar descripcion en formulario create de vacantes

--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -175,7 +175,14 @@
                 Descripción del Puesto:
             </label>
             <div class="editable p-3 bg-gray-100 rounded form-input w-full text-gray-700"></div>
-            <input type="hidden" name="descripcion" id="descripcion">
+            <input type="hidden" name="descripcion" id="descripcion" value="{{ old('descripcion') }}">
+
+            @error('descripcion')
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-3 mb-6" role="alert">
+                    <strong class="font-bold">Error!</strong>
+                    <span class="block">{{ $message }}</span>
+                </div>
+            @enderror
         </div>
 
         <div class="mb-5">
@@ -223,7 +230,7 @@
             // Medium Editor
             const editor = new MediumEditor('.editable', {
                 toolbar: {
-                    buttons: ['bold', 'italic', 'underline', 'quote', 'anchor', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'orderedList', 'unorderedList', 'h2', 'h3'],
+                    buttons: ['bold', 'italic', 'underline', 'quote', 'anchor', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'orderedlist', 'unorderedlist', 'h2', 'h3'],
                     static: true,
                     sticky: true
                 },
@@ -232,10 +239,14 @@
                 }
             })
 
+            // Agrega al input hidden lo que el usuario escribe en medium editor
             editor.subscribe('editableInput', function (eventObj, editable) {
                 const contenido = editor.getContent()
                 document.querySelector('#descripcion').value = contenido
             })
+
+            // llena el editor con el contenido del input hidden
+            editor.setContent(document.querySelector('#descripcion').value)
 
             // Dropzone
             const dropzoneDevJobs = new Dropzone('#dropzoneDevJobs', {
