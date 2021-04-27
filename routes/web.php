ecd7e0223755678422664f303f747c5f5b236a34
Cesar Acual <augustoacual@gmail.com>
2021-04-26 18:43:41 -0600

    Filtrar vacantes por categoría

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,9 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 // Página de inicio
 Route::get('/', 'InicioController')->name('inicio');
 
+// Categorías
+Route::get('/categorias/{categoria}', 'CategoriaController@show')->name('categorias.show');
+
 // Enviar datos para una vacante
 Route::get('/candidatos/{id}', 'CandidatoController@index')->name('candidatos.index');
 Route::post('/candidatos/store', 'CandidatoController@store')->name('candidatos.store');
